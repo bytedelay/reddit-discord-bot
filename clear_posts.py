71bda4bd5fa16410ebd6e9d5c5800c6a289e2b23
bytedelay <rohitprasanna64@yahoo.com>
2026-06-20 22:27:06 +0100

    Clear already available in database

---
+++ clear_posts.py
@@ -0,0 +1,21 @@
+from db import get_connection
+
+
+def clear_posts_for_subreddit(subreddit_name: str):
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+                """
+                DELETE FROM posted_posts
+                WHERE subreddit_name = %s;
+                """,
+                (subreddit_name,),
+            )
+
+            deleted_count = cursor.rowcount
+
+    print(f"Deleted {deleted_count} posts from r/{subreddit_name}.")
+
+
+if __name__ == "__main__":
+    clear_posts_for_subreddit("wallpapers")
